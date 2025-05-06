0cb6fc0ca2beda10664b85bf723906314f58d5f9
Remy Degenne <remydegenne@gmail.com>
2025-05-06 13:02:13 +0200

    min imports

--- EValues/EValue.lean
+++ EValues/EValue.lean
@@ -3,7 +3,11 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import Mathlib
+import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.MeasureTheory.Integral.Bochner.Basic
+import Mathlib.MeasureTheory.VectorMeasure.Decomposition.Jordan
+import Mathlib.Order.CompletePartialOrder
+import Mathlib.Probability.Kernel.Composition.MeasureComp
 
 /-!
 # E-variables
@@ -22,14 +26,11 @@ import Mathlib
 
 open scoped ENNReal
 
+open MeasureTheory ProbabilityTheory
+
 namespace MeasureTheory
 
-variable {Ω : Type*} {mΩ : MeasurableSpace Ω} {μ : Measure Ω}
-  {S : Set (Measure Ω)}
-
-structure IsEVar (X : Ω → ℝ≥0∞) (S : Set (Measure Ω)) : Prop where
-  measurable : Measurable X
-  integrable : ∀ μ ∈ S, ∫⁻ ω, X ω ∂μ ≤ 1
+variable {Ω : Type*} {mΩ : MeasurableSpace Ω} {μ : Measure Ω} {S : Set (Measure Ω)}
 
 noncomputable
 def pairingFun (s : SignedMeasure Ω) (f : Ω → ℝ) : ℝ :=
@@ -47,3 +48,16 @@ def integrableSignedMeasures (L : Set (Ω → ℝ)) :=
 -- positive and negative parts. That is, both parts are in `integrableMeasures L`.
 
 end MeasureTheory
+
+namespace ProbabilityTheory
+
+variable {𝓧 : Type*} {m𝓧 : MeasurableSpace 𝓧} {μ : Measure 𝓧} {S : Set (Measure 𝓧)}
+
+structure IsEVar (X : 𝓧 → ℝ≥0∞) (S : Set (Measure 𝓧)) : Prop where
+  measurable : Measurable X
+  lintegral_le_one : ∀ μ ∈ S, ∫⁻ ω, X ω ∂μ ≤ 1
+
+structure IsRandEVar (κ : Kernel 𝓧 ℝ≥0∞) (S : Set (Measure 𝓧)) : Prop where
+  lintegral_le_one : ∀ μ ∈ S, ∫⁻ ω, ω ∂(κ ∘ₘ μ) ≤ 1
+
+end ProbabilityTheory
