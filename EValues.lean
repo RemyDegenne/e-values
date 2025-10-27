f967529904de71e598420641f3172782d1b09c06
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-10-27 22:35:00 +0100

    Update EValues.lean

--- EValues.lean
+++ EValues.lean
@@ -4,7 +4,6 @@ import EValues.Mathlib.Convex
 import EValues.Mathlib.EDiv
 import EValues.Mathlib.ENNReal
 import EValues.Mathlib.Jensen
-import EValues.Numeraire
 import EValues.NumeraireExistence
 import EValues.RealPolar
 import EValues.Utility
