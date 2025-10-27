cb8fa26dc167fc36485a25398159f54481668290
Remy Degenne <remydegenne@gmail.com>
2025-10-27 22:16:15 +0100

    golf, review

--- EValues/Numeraire.lean
+++ EValues/Numeraire.lean
@@ -46,88 +46,69 @@ namespace IsNumeraire
 variable {X Y : 𝓧 → ℝ≥0∞} {μ : Measure 𝓧} [IsProbabilityMeasure μ] {S : Set (Measure 𝓧)}
   {hS : ∀ μ ∈ S, IsProbabilityMeasure μ}
 
-lemma ae_pos (hX : IsNumeraire X hS μ) : ∀ᵐ ω ∂μ, X ω ≠ 0 := by
-  by_contra h
-  suffices ∫⁻ ω, (X ω)⁻¹ ∂μ = ⊤ by
-    have lintegral_ratio_le_one := hX.lintegral_ratio_le_one (isEVar_one S hS)
-    simp only [Pi.one_apply, one_div, this] at lintegral_ratio_le_one
-    contradiction
-  refine lintegral_eq_top_of_measure_eq_top_ne_zero hX.measurable.inv.aemeasurable ?_
-  unfold Filter.Eventually at h
-  simp [MeasureTheory.ae] at h
-  suffices {ω | X ω ≠ 0}ᶜ = {ω | (X ω)⁻¹ = ⊤} by
-    rwa [← this]
-  ext ω
-  simp
+lemma lintegral_inv_le_one (hX : IsNumeraire X hS μ) : ∫⁻ ω, (X ω)⁻¹ ∂μ ≤ 1 := by
+  simpa using hX.lintegral_ratio_le_one (isEVar_one S hS)
 
-lemma lintegral_eq_lintegral_on_fsupport (hX : IsNumeraire X hS μ)
-    (hY : IsEVar Y S) : ∫⁻ ω, Y ω / X ω ∂μ = ∫⁻ ω in X.fsupport, Y ω / X ω ∂μ := by
+lemma ae_pos (hX : IsNumeraire X hS μ) : ∀ᵐ ω ∂μ, 0 < X ω := by
+  suffices ∀ᵐ ω ∂μ, (X ω)⁻¹ < ∞ by
+    simp only [pos_iff_ne_zero]
+    filter_upwards [this] with ω hω using by simpa using hω.ne
+  refine ae_lt_top ?_ ?_
+  · have := hX.measurable
+    fun_prop
+  · exact ne_top_of_le_ne_top (by simp) (hX.lintegral_inv_le_one)
+
+lemma ae_ne_zero (hX : IsNumeraire X hS μ) : ∀ᵐ ω ∂μ, X ω ≠ 0 := by
+  filter_upwards [hX.ae_pos] with ω hω using hω.ne'
+
+lemma lintegral_eq_setLIntegral_fsupport (hX : IsNumeraire X hS μ) (hY : IsEVar Y S) :
+    ∫⁻ ω, Y ω / X ω ∂μ = ∫⁻ ω in X.fsupport, Y ω / X ω ∂μ := by
   rw [← lintegral_add_compl _ hX.measurable_fsupport]
-  suffices ∫⁻ ω in X.fsupportᶜ, Y ω / X ω ∂μ = 0 by
-    simp [this]
-  rw [setLIntegral_eq_zero_iff (hX.measurable_fsupport).compl
-    <| hY.measurable.div hX.measurable]
-  filter_upwards [hX.ae_pos] with ω hω hω₂
-  rw [X.fsupport_compl] at hω₂
-  rcases hω₂ with hω₂ | hω₂
-  · simp_all
-  · contradiction
+  suffices ∫⁻ ω in X.fsupportᶜ, Y ω / X ω ∂μ = 0 by simp [this]
+  rw [setLIntegral_eq_zero_iff (hX.measurable_fsupport).compl <| hY.measurable.div hX.measurable]
+  filter_upwards [hX.ae_ne_zero] with ω hω hω₂
+  simp only [fsupport, ne_eq, mem_compl_iff, mem_inter_iff, mem_setOf_eq, hω, not_false_eq_true,
+    and_true, Decidable.not_not] at hω₂
+  simp [hω₂]
 
 lemma ae_top_implies_numeraire_top (hX : IsNumeraire X hS μ) (hY : IsEVar Y S) :
-    ∀ᵐ ω ∂μ, Y ω = ⊤ → X ω = ⊤ := by
+    ∀ᵐ ω ∂μ, Y ω = ∞ → X ω = ∞ := by
   by_contra h
-  unfold Filter.Eventually at h
-  replace h : μ {ω | Y ω = ⊤ ∧ X ω ≠ ⊤} ≠ 0 := by
-    suffices {ω | Y ω = ⊤ ∧ X ω ≠ ⊤} = {ω | Y ω = ⊤ → X ω = ⊤}ᶜ by
-      rw [this]
-      exact h
-    ext ω
-    simp
-  have m : MeasurableSet {ω | Y ω = ⊤ ∧ X ω ≠ ⊤} := by
-    suffices MeasurableSet {ω | Y ω = ⊤} ∧ MeasurableSet {ω | X ω ≠ ⊤} from this.1.inter this.2
-    constructor
-    · exact hY.measurable <| measurableSet_singleton ⊤
-    · rw [← MeasurableSet.compl_iff]
-      suffices {ω | X ω ≠ ⊤}ᶜ = {ω | X ω = ⊤} by
-        rw [this]
-        exact hX.measurable <| measurableSet_singleton ⊤
-      ext ω
-      simp
+  simp only [ae_iff, Classical.not_imp] at h
+  have m : MeasurableSet {ω | Y ω = ∞ ∧ X ω ≠ ∞} :=
+    MeasurableSet.inter ((measurableSet_singleton ∞).preimage hY.measurable)
+      ((measurableSet_singleton ∞).compl.preimage hX.measurable)
   have lintegral_ratio_le_one := hX.lintegral_ratio_le_one hY
   rw [← lintegral_add_compl _ m] at lintegral_ratio_le_one
-  suffices ∀ ω ∈ {ω | Y ω = ⊤ ∧ X ω ≠ ⊤}, Y ω / X ω = ⊤ by
+  suffices ∀ ω ∈ {ω | Y ω = ∞ ∧ X ω ≠ ∞}, Y ω / X ω = ∞ by
     rw [setLIntegral_congr_fun m this] at lintegral_ratio_le_one
-    simp only [ne_eq, lintegral_const, MeasurableSet.univ, Measure.restrict_apply,
-      univ_inter] at lintegral_ratio_le_one
-    rw [top_mul h] at lintegral_ratio_le_one
-    contradiction
+    simp [top_mul h] at lintegral_ratio_le_one
   intro ω hω
   simp [hω.1, ENNReal.top_div, hω.2]
 
-lemma lintegral_eq_lintegral_on_rev_fsupport (hX : IsNumeraire X hS μ)
-    (hY : IsEVar Y S) : ∫⁻ ω, Y ω / X ω ∂μ = ∫⁻ ω in Y.fsupport, Y ω / X ω ∂μ := by
+-- `rev`?
+lemma lintegral_eq_setLIntegral_rev_fsupport (hX : IsNumeraire X hS μ) (hY : IsEVar Y S) :
+    ∫⁻ ω, Y ω / X ω ∂μ = ∫⁻ ω in Y.fsupport, Y ω / X ω ∂μ := by
   rw [← lintegral_add_compl _ hY.measurable_fsupport]
-  suffices ∫⁻ ω in Y.fsupportᶜ, Y ω / X ω ∂μ = 0 by
-    simp [this]
+  suffices ∫⁻ ω in Y.fsupportᶜ, Y ω / X ω ∂μ = 0 by simp [this]
   rw [setLIntegral_eq_zero_iff (hY.measurable_fsupport).compl
     <| hY.measurable.div hX.measurable]
   filter_upwards [hX.ae_top_implies_numeraire_top hY] with ω hω hω₂
-  rw [Y.fsupport_compl] at hω₂
-  rcases hω₂ with hω₂ | hω₂
-  · rw [hω₂, hω hω₂]
-    simp
-  · rw [hω₂]
-    simp
+  simp only [ENNReal.div_eq_zero_iff]
+  by_cases hω_top : Y ω = ∞
+  · simp [hω_top, hω hω_top]
+  · simp only [fsupport, ne_eq, mem_compl_iff, mem_inter_iff, mem_setOf_eq, not_and,
+      Decidable.not_not] at hω₂
+    simp [hω_top, hω₂]
 
 lemma measure_fsupport_ne_zero_or_ae_top (hX : IsNumeraire X hS μ) :
-    μ X.fsupport ≠ 0 ∨ X =ᵐ[μ] ⊤ := by
-  by_contra h
-  push_neg at h
+    μ X.fsupport ≠ 0 ∨ X =ᵐ[μ] fun _ ↦ ∞ := by
+  by_contra! h
   have ratio_eq_zero : ∫⁻ ω, X ω / X ω ∂μ = 0 := by
-    rw [hX.lintegral_eq_lintegral_on_fsupport hX.toIsEVar]
+    rw [hX.lintegral_eq_setLIntegral_fsupport hX.toIsEVar]
     exact setLIntegral_measure_zero _ _ h.1
   rw [lintegral_eq_zero_iff <| hX.measurable.div hX.measurable] at ratio_eq_zero
-  have mₜ : MeasurableSet {ω | X ω = ⊤} := hX.measurable <| measurableSet_singleton ⊤
+  have mₜ : MeasurableSet {ω | X ω = ∞} := hX.measurable <| measurableSet_singleton ∞
   have m₀ : MeasurableSet {ω | X ω = 0} := hX.measurable <| measurableSet_singleton 0
   replace ratio_eq_zero : μ X.fsupportᶜ = μ univ := by
     rw [measure_univ, ← prob_compl_eq_zero_iff hX.measurable_fsupport.compl]
@@ -138,35 +119,28 @@ lemma measure_fsupport_ne_zero_or_ae_top (hX : IsNumeraire X hS μ) :
       ENNReal.div_eq_zero_iff, mem_union, mem_setOf_eq]
     exact hω.symm
   rw [X.fsupport_compl, measure_union ?_ m₀] at ratio_eq_zero
-  · have : {ω | X ω = 0} = {ω | X ω ≠ 0}ᶜ := by
-      ext ω
-      simp
-    rw [this, hX.ae_pos, add_zero, measure_univ, ← prob_compl_eq_zero_iff mₜ] at ratio_eq_zero
+  · have : {ω | X ω = 0} = {ω | X ω ≠ 0}ᶜ := by ext; simp
+    rw [this, hX.ae_ne_zero, add_zero, measure_univ, ← prob_compl_eq_zero_iff mₜ] at ratio_eq_zero
     exact h.2 ratio_eq_zero
-  · rw [disjoint_iff_inter_eq_empty]
-    ext ω
-    simp only [mem_inter_iff, mem_setOf_eq, mem_empty_iff_false, iff_false, not_and]
-    intro hω
-    rw [hω]
-    simp
+  · exact Disjoint.preimage X (s := {∞}) (t := {0}) (by simp)
 
 lemma inv_lintegral_eq_one (hX : IsNumeraire X hS μ) (hY : IsNumeraire Y hS μ)
     (h : μ X.fsupport ≠ 0) : (∫⁻ ω, Y ω / X ω ∂μ)⁻¹ = 1 := by
   set W := Y / X
-  rw [hX.lintegral_eq_lintegral_on_fsupport hY.toIsEVar]
+  rw [hX.lintegral_eq_setLIntegral_fsupport hY.toIsEVar]
   suffices 1 ≤ (∫⁻ ω in X.fsupport, W ω ∂μ)⁻¹ by
     refine le_antisymm ?_ this
     suffices (∫⁻ ω in X.fsupport, W ω ∂μ)⁻¹ ≤ ∫⁻ ω in X.fsupport, (W ω)⁻¹ ∂μ by
       trans ∫⁻ ω in X.fsupport, (W ω)⁻¹ ∂μ
       · assumption
       · rw [setLIntegral_congr_fun hX.measurable_fsupport <| ENNReal.inv_div_fsupport Y X]
-        rw [← hY.lintegral_eq_lintegral_on_rev_fsupport hX.toIsEVar]
+        rw [← hY.lintegral_eq_setLIntegral_rev_fsupport hX.toIsEVar]
         exact hY.lintegral_ratio_le_one hX.toIsEVar
     refine strictConvexOn_inv.convexOn.map_set_lintegral_le continuousOn_inv ?_ h ?_ ?_
     · exact isClosed_univ
     · simp
     · simp
-  simp only [Pi.div_apply, ← hX.lintegral_eq_lintegral_on_fsupport hY.toIsEVar, le_inv_iff_mul_le,
+  simp only [Pi.div_apply, ← hX.lintegral_eq_setLIntegral_fsupport hY.toIsEVar, le_inv_iff_mul_le,
     one_mul, W]
   exact hX.lintegral_ratio_le_one hY.toIsEVar
 
@@ -176,11 +150,11 @@ lemma ae_unique (hX : IsNumeraire X hS μ) (hY : IsNumeraire Y hS μ) : X =ᵐ[�
   rcases hY.measure_fsupport_ne_zero_or_ae_top with μ_fsupport | hYₜ
   · let W := X / Y
     have inv_avg_eq_one : (∫⁻ ω in Y.fsupport, W ω ∂μ)⁻¹ = 1 := by
-      simp only [Pi.div_apply, ← hY.lintegral_eq_lintegral_on_fsupport hX.toIsEVar, W]
+      simp only [Pi.div_apply, ← hY.lintegral_eq_setLIntegral_fsupport hX.toIsEVar, W]
       exact hY.inv_lintegral_eq_one hX μ_fsupport
     have avg_eq_one : ∫⁻ ω, W ω ∂μ = 1 := by
       simp only [Pi.div_apply, ENNReal.inv_eq_one, W] at inv_avg_eq_one
-      rwa [← hY.lintegral_eq_lintegral_on_fsupport hX.toIsEVar] at inv_avg_eq_one
+      rwa [← hY.lintegral_eq_setLIntegral_fsupport hX.toIsEVar] at inv_avg_eq_one
     rcases hY.measure_fsupport_ne_zero_or_ae_top with hY₀ | hYₜ
     · have strict_Jensen : W =ᵐ[μ] const 𝓧 (∫⁻ ω, W ω ∂μ) ∨
           (∫⁻ ω in Y.fsupport, W ω ∂μ)⁻¹ < ∫⁻ ω in Y.fsupport, (W ω)⁻¹ ∂μ := by
@@ -194,10 +168,10 @@ lemma ae_unique (hX : IsNumeraire X hS μ) (hY : IsNumeraire Y hS μ) : X =ᵐ[�
         simp only [Pi.div_apply, const_apply, W] at hx
         exact ENNReal.eq_of_div_eq_one hx
       · exfalso
-        rw [inv_avg_eq_one] at h
-        rw [setLIntegral_congr_fun hY.measurable_fsupport <| ENNReal.inv_div_fsupport X Y] at h
+        rw [inv_avg_eq_one,
+          setLIntegral_congr_fun hY.measurable_fsupport <| ENNReal.inv_div_fsupport X Y] at h
         refine h.not_ge ?_
-        rw [← hX.lintegral_eq_lintegral_on_rev_fsupport hY.toIsEVar]
+        rw [← hX.lintegral_eq_setLIntegral_rev_fsupport hY.toIsEVar]
         exact hX.lintegral_ratio_le_one hY.toIsEVar
     · suffices ∀ᵐ ω ∂μ, W ω = 0 by
         simp [lintegral_congr_ae this] at avg_eq_one
