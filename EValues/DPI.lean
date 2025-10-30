9af1f6d1cb4f8f9e1e0f319fc6dd4e665fb4a127
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-10-30 19:08:15 +0100

    imports

--- EValues/DPI.lean
+++ EValues/DPI.lean
@@ -1,9 +1,8 @@
 /-
 Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne
+Authors: Rémy Degenne, Gaëtan Serré
 -/
-import Mathlib
 import EValues.EValue
 import EValues.Utility
 
