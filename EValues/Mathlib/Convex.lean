13c84dfd1dd07a35b44776699f16b685045732a6
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-11-05 17:58:25 +0100

    First attempt log convex

--- EValues/Mathlib/Convex.lean
+++ EValues/Mathlib/Convex.lean
@@ -4,10 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gaëtan Serré
 -/
 
-import Mathlib.Algebra.Order.Module.OrderedSMul
-import Mathlib.Analysis.Convex.Slope
-import Mathlib.Analysis.InnerProductSpace.Basic
-import Mathlib.Analysis.SpecialFunctions.Log.ENNRealLog
+import Mathlib
 
 open Set ENNReal NNReal
 
@@ -75,7 +72,7 @@ lemma strictConvexOn_inv : StrictConvexOn ℝ≥0∞ univ <| Inv.inv (α := ℝ�
       simp only [x₀, mul_zero, zero_add, ENNReal.inv_zero, ENNReal.mul_top a₀.ne', top_add,
         inv_lt_top, CanonicallyOrderedAdd.mul_pos]
       refine ⟨b₀, pos_of_ne_zero ?_⟩
-      rw [x₀] at hxy
+      rw [← x₀]
       exact hxy.symm
     · push_neg at xₜ
       simp only [xₜ, ENNReal.mul_top a₀.ne', top_add, inv_top, mul_zero, zero_add,
@@ -103,5 +100,92 @@ lemma convexOn_inv : ConvexOn ℝ≥0∞ univ <| Inv.inv (α := ℝ≥0∞) := s
 noncomputable instance : SMul ℝ≥0 EReal where smul c x := c * x
 noncomputable instance : SMul ℝ≥0∞ EReal where smul c x := c * x
 
-lemma ConcaveOn_log : ConcaveOn ℝ≥0 univ ENNReal.log := by
+lemma test {a b : EReal} (h1 : a ≠ ⊤) (h2 : b ≠ ⊤) (h3 : a ≠ ⊥) (h4 : b ≠ ⊥)
+    (h5 : b.toReal ≤ a.toReal) : b ≤ a := by
+  lift a to ℝ using ⟨h1, h3⟩
+  lift b to ℝ using ⟨h2, h4⟩
+  exact EReal.coe_le_coe_iff.mpr h5
+
+lemma EReal.toReal_log {x : ℝ≥0∞} (hx₀ : x ≠ 0) (hxₜ : x ≠ ⊤) :
+    (ENNReal.log x).toReal = Real.log x.toReal := by
   sorry
+
+lemma ConcaveOn_log : ConcaveOn ℝ≥0∞ univ ENNReal.log := by
+  refine ⟨convex_univ, ?_⟩
+  intro x _ y _ a b a₀ b₀ hab
+  simp only [smul_eq_mul]
+  have aₜ : a ≠ ⊤ := by
+    by_contra h
+    simp [h] at hab
+  have bₜ : b ≠ ⊤ := by
+    by_contra h
+    simp [h] at hab
+  have smul_log : ∀ (x y : ℝ≥0∞), x • y.log = x * y.log := fun _ _ ↦ rfl
+  simp only [smul_log]
+  by_cases h : x ≠ 0 ∧ x ≠ ⊤ ∧ y ≠ 0 ∧ y ≠ ⊤
+  · obtain ⟨x₀, xₜ, y₀, yₜ⟩ := h
+    suffices (a * x.log + b * y.log).toReal ≤ ((a * x + b * y).log).toReal by
+      refine test ?_ ?_ ?_ ?_ this
+      any_goals sorry
+    rw [EReal.toReal_add, EReal.toReal_mul, EReal.toReal_mul, EReal.toReal_log, EReal.toReal_log,
+      EReal.toReal_log, toReal_add, toReal_mul, toReal_mul]
+    · simp only [EReal.toReal_coe_ennreal]
+      obtain ⟨_, conv⟩ := strictConcaveOn_log_Ioi.concaveOn
+      simp only [smul_eq_mul] at conv
+      refine conv (toReal_pos x₀ xₜ) (toReal_pos y₀ yₜ) toReal_nonneg toReal_nonneg ?_
+      rw [← toReal_add aₜ bₜ]
+      exact (toReal_eq_one_iff (a + b)).mpr hab
+    · exact mul_ne_top aₜ xₜ
+    · exact mul_ne_top bₜ yₜ
+    · sorry
+    · refine add_ne_top.mpr ⟨?_, ?_⟩
+      · exact mul_ne_top aₜ xₜ
+      · exact mul_ne_top bₜ yₜ
+    · exact y₀
+    · exact yₜ
+    · exact x₀
+    · exact xₜ
+    · refine (EReal.mul_ne_top _ _).mpr ?_
+      sorry
+    · refine (EReal.mul_ne_bot _ _).mpr ?_
+      sorry
+    · refine (EReal.mul_ne_top _ _).mpr ?_
+      sorry
+    · refine (EReal.mul_ne_bot _ _).mpr ?_
+      sorry
+  · simp only [not_and_or] at h
+    rcases h with x₀ | xₜ | y₀ | yₜ
+    · push_neg at x₀
+      by_cases a_eq₀ : a = 0
+      · simp [a_eq₀]
+        sorry
+      · push_neg at a_eq₀
+        replace a₀ : 0 < a := pos_of_ne_zero a_eq₀
+        rw [x₀, log_zero, EReal.mul_bot_of_pos <| EReal.coe_ennreal_pos.mpr a₀]
+        simp
+    · push_neg at xₜ
+      by_cases a_eq₀ : a = 0
+      · simp [a_eq₀]
+        sorry
+      · push_neg at a_eq₀
+        replace a₀ : 0 < a := pos_of_ne_zero a_eq₀
+        rw [xₜ, log_top, EReal.mul_top_of_pos <| EReal.coe_ennreal_pos.mpr a₀]
+        rw [ENNReal.mul_top a₀.ne']
+        simp
+    · push_neg at y₀
+      by_cases b_eq₀ : b = 0
+      · simp [b_eq₀]
+        sorry
+      · push_neg at b_eq₀
+        replace b₀ : 0 < b := pos_of_ne_zero b_eq₀
+        rw [y₀, log_zero, EReal.mul_bot_of_pos <| EReal.coe_ennreal_pos.mpr b₀]
+        simp
+    · push_neg at yₜ
+      by_cases b_eq₀ : b = 0
+      · simp [b_eq₀]
+        sorry
+      · push_neg at b_eq₀
+        replace b₀ : 0 < b := pos_of_ne_zero b_eq₀
+        rw [yₜ, log_top, EReal.mul_top_of_pos <| EReal.coe_ennreal_pos.mpr b₀]
+        rw [ENNReal.mul_top b₀.ne']
+        simp
