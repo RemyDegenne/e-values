97646ba4ead8ed5e66cf931a1eb3383335113531
Gaëtan Serré <56162277+gaetanserre@users.noreply.github.com>
2025-11-14 14:23:13 +0100

    Update EValues/Mathlib/EReal.lean
    Co-authored-by: Rémy Degenne <remydegenne@gmail.com>

--- EValues/Mathlib/EReal.lean
+++ EValues/Mathlib/EReal.lean
@@ -8,7 +8,7 @@ import Mathlib.Analysis.SpecialFunctions.Log.ENNRealLog
 
 open ENNReal
 
-lemma EReal.le_real_imp_le {a b : EReal} (h1 : a ≠ ⊤) (h2 : b ≠ ⊤) (h3 : a ≠ ⊥) (h4 : b ≠ ⊥)
+lemma EReal.le_of_toReal_le {a b : EReal} (h1 : a ≠ ⊤) (h2 : b ≠ ⊤) (h3 : a ≠ ⊥) (h4 : b ≠ ⊥)
     (h5 : b.toReal ≤ a.toReal) : b ≤ a := by
   lift a to ℝ using ⟨h1, h3⟩
   lift b to ℝ using ⟨h2, h4⟩
