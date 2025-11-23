a33be2eaba69f3be6c7aa40a25510801512122b8
Gaëtan Serré <gaetan.serre@ens-paris-saclay.fr>
2025-11-23 23:07:58 +0100

    almost second part of `erenyiDiv_bounded_eq_erenyiDiv_bernoulli`

--- EValues/ERenyi.lean
+++ EValues/ERenyi.lean
@@ -7,6 +7,7 @@ import EValues.DPI
 import EValues.Product
 import EValues.Mathlib.iSup
 import EValues.Mathlib.unitInterval
+import Mathlib.MeasureTheory.Measure.GiryMonad
 
 /-!
 # E-Rényi divergence
@@ -551,9 +552,82 @@ lemma erenyiDiv_bounded_eq_erenyiDiv_bernoulli {a b : ℝ} (ha : a ∈ I) (hb : 
         · have := hμ.2
           rw [integral_map (by fun_prop) (by fun_prop)]
           simp_all [doubleton]
-  · let κ : Kernel I ({0, 1} : Set ℝ) := by sorry
-    haveI : IsMarkovKernel κ := by sorry
-    have : B₁ = {κ ∘ₘ μ | μ ∈ D₁} := by sorry
+  · let Ber (p : I) : Measure ({0, 1} : Set ℝ) :=
+      (ENNReal.ofReal p) • Measure.dirac ⟨1, by simp⟩ +
+        (ENNReal.ofReal (1 - p)) • Measure.dirac ⟨0, by simp⟩
+
+    haveI Ber_is_prob : ∀ p, IsProbabilityMeasure (Ber p) := by
+      intro p
+      refine isProbabilityMeasure_iff.mpr ?_
+      simp only [Measure.coe_add, Measure.coe_smul, Pi.add_apply, Pi.smul_apply, measure_univ,
+        smul_eq_mul, mul_one, Ber]
+      rw [← ENNReal.ofReal_add (by unit_interval) (by unit_interval)]
+      simp
+
+    let κ : Kernel I ({0, 1} : Set ℝ) := ⟨Ber, by fun_prop⟩
+    haveI : IsMarkovKernel κ := ⟨Ber_is_prob⟩
+
+    have : B₁ = {κ ∘ₘ μ | μ ∈ D₁} := by
+      ext μ
+      constructor
+      · rintro ⟨hμ_prob, hμ_int⟩
+        refine ⟨μ.map doubleton, ?_, ?_⟩
+        · refine ⟨μ.isProbabilityMeasure_map measurable_doubleton.aemeasurable, ?_⟩
+          rw [integral_map (by fun_prop) (by fun_prop)]
+          simp_all [doubleton]
+        · ext S hS
+          rw [Measure.bind_apply hS (by fun_prop), lintegral_map]
+          · rw [doubleton_lintegral]
+            simp only [doubleton, Set.Icc.mk_zero, Kernel.coe_mk, Set.Icc.coe_zero,
+              ENNReal.ofReal_zero, zero_smul, sub_zero, ENNReal.ofReal_one, one_smul, zero_add,
+              Measure.dirac_apply, Set.Icc.mk_one, Set.Icc.coe_one, sub_self, add_zero, κ, Ber]
+            by_cases h0 : ⟨0, by simp⟩ ∈ S <;> by_cases h1 : ⟨1, by simp⟩ ∈ S
+            · have : S = Set.univ := by grind
+              rw [this]
+              simp_all only [Set.mem_Icc, Subtype.forall, forall_and_index, MeasurableSet.univ,
+                Set.mem_univ, Set.indicator_of_mem, Pi.one_apply, mul_one, measure_univ]
+              rw [← measure_union (by simp) (by simp), doubleton_union_univ]
+              simp
+            · have : S = {⟨0, by simp⟩} := by grind
+              simp_all
+            · have : S = {⟨1, by simp⟩} := by grind
+              simp_all
+            · have : S = ∅ := by grind
+              simp_all
+          · exact Kernel.measurable_coe κ hS
+          · fun_prop
+      · rintro ⟨ν, hν, rfl⟩
+        refine ⟨?_, ?_⟩
+        · have := hν.1
+          infer_instance
+        · obtain ⟨hν_prob, hν⟩ := hν
+          rw [integral_eq_lintegral_of_nonneg_ae] at ⊢ hν
+          · rw [Measure.lintegral_bind]
+            trans (∫⁻ (x : I), ENNReal.ofReal x ∂ν).toReal
+            · gcongr with b
+              · apply ne_of_lt
+                calc
+                _ ≤ ∫⁻ x, 1 ∂ν := by
+                  refine lintegral_mono fun x ↦ ?_
+                  simp only [ENNReal.ofReal_le_one]
+                  unit_interval
+                _ = (1 : ℝ≥0∞) := by simp
+                _ < ⊤ := by simp
+              · rw [doubleton_lintegral]
+                simp [κ, Ber]
+            · exact hν
+            · fun_prop
+            · fun_prop
+          · apply ae_of_all
+            intro x
+            cases x.2 with
+            | inl hx0 => simp_all
+            | inr hx1 => simp_all
+          · fun_prop
+          · apply ae_of_all
+            intro x
+            unit_interval
+          · fun_prop
     rw [this]
     clear this
     have : B₂ = {κ ∘ₘ μ | μ ∈ D₂} := by sorry
--- EValues/Mathlib/unitInterval.lean
+++ EValues/Mathlib/unitInterval.lean
@@ -4,27 +4,39 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Gaëtan Serré
 -/
 
-import Mathlib.Analysis.RCLike.Basic
+import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.MeasureTheory.Constructions.Polish.Basic
+import Mathlib.MeasureTheory.Integral.Bochner.Basic
 import Mathlib.Topology.EMetricSpace.Paracompact
 import Mathlib.Topology.Separation.CompletelyRegular
 
-open unitInterval
+open unitInterval MeasureTheory ENNReal
 
 lemma doubleton_eq (s : Set ({0, 1} : Set ℝ)) :
     s = ∅ ∨ s = {⟨0, by simp⟩} ∨ s = {⟨1, by simp⟩} ∨ s = {⟨0, by simp⟩, ⟨1, by simp⟩} := by
+  grind
+
+lemma doubleton_univ : ({⟨0, by simp⟩, ⟨1, by simp⟩} : Set ({0, 1} : Set ℝ)) = Set.univ := by
+  grind
+
+lemma doubleton_union_univ : ({⟨0, by simp⟩} : Set ({0, 1} : Set ℝ)) ∪ {⟨1, by simp⟩}
+    = Set.univ := by
+  grind
+
+lemma doubleton_integral {μ : Measure ({0, 1} : Set ℝ)} {f : ({0, 1} : Set ℝ) → ℝ}
+  (hf : Integrable f μ) : ∫ x, f x ∂μ =
+    μ.real {⟨0, by simp⟩} * f ⟨0, by simp⟩ + μ.real {⟨1, by simp⟩} * f ⟨1, by simp⟩ := by
+  rw [integral_countable' hf, tsum_fintype]
+  sorry
+
+lemma doubleton_lintegral {μ : Measure ({0, 1} : Set ℝ)} {f : ({0, 1} : Set ℝ) → ℝ≥0∞}
+    : ∫⁻ x, f x ∂μ = μ {⟨0, by simp⟩} * f ⟨0, by simp⟩ + μ {⟨1, by simp⟩} * f ⟨1, by simp⟩ := by
+  rw [lintegral_countable' f, tsum_fintype]
   sorry
 
 namespace unitInterval
 
-lemma zero_one_mem {x : ({0, 1} : Set ℝ)} : x.1 ∈ I := by
-  obtain ⟨x, hx⟩ := x
-  cases hx with
-  | inl hx =>
-    simp_all
-  | inr hx =>
-    simp_all only [Set.mem_Icc]
-    simp_all
+lemma zero_one_mem {x : ({0, 1} : Set ℝ)} : x.1 ∈ I := by grind
 
 /-- Define the embedding from the doubleton set {0, 1} into the unit interval I -/
 def doubleton (x : ({0, 1} : Set ℝ)) := (⟨x.1, zero_one_mem⟩ : I)
@@ -38,32 +50,27 @@ lemma doubleton_emb : MeasurableEmbedding doubleton where
   measurable := by fun_prop
   measurableSet_image' := by
     intro s hs
-    simp only [Set.image]
-    by_cases hsₐ : s = ∅
-    · simp [hsₐ]
-    · by_cases hs₀ : s = {⟨0, by simp⟩}
-      · simp [hs₀]
-      · by_cases hs₁ : s = {⟨1, by simp⟩}
-        · simp [hs₁]
-        · suffices {x | ∃ a ∈ s, ⟨a, zero_one_mem⟩ = x} = ({0, 1} : Set I) by
-            unfold doubleton
-            rw [this]
-            simp
-          push_neg at hsₐ hs₀ hs₁
-          have : s = {⟨0, by simp⟩, ⟨1, by simp⟩} := by
-            by_contra hc
-            push_neg at hc
-            --have h := ⟨hsₐ, hs₀, hs₁, hc⟩
-            cases doubleton_eq s with
-            | inl hs_empty => simp_all
-            | inr h =>
-              cases h with
-              | inl hs_single_0 => simp_all
-              | inr h =>
-                cases h with
-                | inl hs_single_1 => simp_all
-                | inr hs_both => simp_all
-          aesop
+    by_cases h0 : ⟨0, by simp⟩ ∈ s <;> by_cases h1 : ⟨1, by simp⟩ ∈ s
+    · suffices doubleton '' s = ({0, 1} : Set I) by simp [this]
+      have : s = Set.univ := by grind
+      simp only [doubleton, this, Set.image_univ]
+      ext x
+      simp only [Set.mem_range, Subtype.exists, Set.mem_insert_iff, Set.mem_singleton_iff]
+      by_cases hx : x = 0
+      · simp only [hx, zero_ne_one, or_false, iff_true]
+        exact ⟨0, by simp, rfl⟩
+      · simp only [hx, false_or]
+        refine ⟨fun ⟨a, ha, h_eq⟩ ↦ ?_, fun h ↦ ⟨1, by simp, h.symm⟩⟩
+        rw [← h_eq]
+        rcases ha with rfl | rfl
+        · simp_all
+        · rfl
+    · have : s = {⟨0, by simp⟩} := by grind
+      simp [this]
+    · have : s = {⟨1, by simp⟩} := by grind
+      simp [this]
+    · have : s = ∅ := by grind
+      simp [this]
 
 @[fun_prop]
 lemma measurable_doubleton : Measurable doubleton := doubleton_emb.measurable
@@ -80,11 +87,6 @@ lemma measurable_doubleton_inv : Measurable doubleton_inv := by
 lemma doubleton_left_inv : Function.LeftInverse doubleton_inv doubleton := by
   intro x
   simp only [doubleton, doubleton_inv]
-  obtain ⟨x, hx⟩ := x
-  cases hx with
-  | inl hx => simp_all
-  | inr hx =>
-    rw [Set.mem_singleton_iff] at hx
-    simp_all
+  grind
 
 end unitInterval
