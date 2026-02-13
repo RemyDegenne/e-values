95c5b9618b4269c70ff5e42ae43dc0695703c48e
Remy Degenne <remydegenne@gmail.com>
2026-02-13 15:37:01 +0100

    work

--- EValues/NumeraireExistence.lean
+++ EValues/NumeraireExistence.lean
@@ -32,12 +32,40 @@ section
 
 variable {U : ℝ≥0∞ → EReal}
 
+lemma eintegrable_of_le {f : 𝓧 → EReal} {b : EReal} (hf : ∀ x, f x ≤ b) (hb : b ≠ ⊤) (P : Measure 𝓧)
+    [IsFiniteMeasure P] :
+    eintegrable f P := by
+  refine .inl (ne_of_lt ?_)
+  calc ∫⁻ x, (f x).toENNReal ∂P
+  _ ≤ ∫⁻ x, b.toENNReal ∂P := by
+    gcongr
+    exact EReal.toENNReal_le_toENNReal (hf _) -- missing gcongr
+  _ = b.toENNReal * P .univ := by simp [lintegral_const]
+  _ < ⊤ := by simp [hb, lt_top_iff_ne_top, ENNReal.mul_eq_top]
+
+lemma eintegral_lt_top_of_le {f : 𝓧 → EReal} {b : EReal} (hf : ∀ x, f x ≤ b) (hb : b ≠ ⊤)
+    (P : Measure 𝓧) [IsFiniteMeasure P] :
+    ∫ᵉ x, f x ∂P < ⊤ := by
+  rw [eintegral]
+  calc (∫⁻ x, (f x).toENNReal ∂P : EReal) - ∫⁻ x, (-f x).toENNReal ∂P
+  _ ≤ ∫⁻ x, (f x).toENNReal ∂P - 0 := EReal.sub_le_sub le_rfl (by positivity)
+  _ ≤ ∫⁻ x, b.toENNReal ∂P := by
+    simp only [sub_zero]
+    refine EReal.coe_ennreal_le_coe_ennreal_iff.mpr ?_ -- missing gcongr
+    gcongr
+    exact EReal.toENNReal_le_toENNReal (hf _)
+  _ = b.toENNReal * P .univ := by simp [lintegral_const]
+  _ < ⊤ := by
+    norm_cast
+    rw [lt_top_iff_ne_top, ne_eq, EReal.coe_ennreal_eq_top_iff]
+    simp [hb, ENNReal.mul_eq_top]
+
 -- needs only two things about IsEVar: convex and closed under a.e. limits
 /-- There exists a utility-maximizing e-variable which is infinite whenever another e-variable
 is infinite. -/
 lemma exists_eq_iSup_eintegral_of_le' (hU_ccv : ConcaveOn ℝ≥0 Set.univ U)
-    {b : ℝ} (hU_cont : Continuous U) (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
+    {B : ℝ} (hU_cont : Continuous U) (hU_le : ∀ x : ℝ≥0∞, U x ≤ B)
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
     ∃ Y : 𝓧 → ℝ≥0∞, IsEVar Y S ∧ ∀ X, IsEVar X S → ∫ᵉ x, U (X x) ∂P ≤ ∫ᵉ x, U (Y x) ∂P := by
   let S' := {y | ∃ X, IsEVar X S ∧ y = ∫ᵉ x, U (X x) ∂P}
   have hS' : S'.Nonempty := ⟨∫ᵉ x, U 1 ∂P, ⟨1, isEVar_one _, rfl⟩⟩
@@ -94,6 +122,8 @@ lemma exists_eq_iSup_eintegral_of_le' (hU_ccv : ConcaveOn ℝ≥0 Set.univ U)
         simp only [Pi.add_apply, Pi.smul_apply, smul_eq_mul] at hY hZ ⊢
         have ha_ne_top : a ≠ ∞ := fun ha_top ↦ by simp [ha_top] at hab
         have hb_ne_top : b ≠ ∞ := fun hb_top ↦ by simp [hb_top] at hab
+        have h_int (Y : 𝓧 → ℝ≥0∞) : eintegrable (fun ω ↦ U (Y ω)) P :=
+          eintegrable_of_le (fun _ ↦ hU_le _) (by simp) P
         calc ∫ᵉ ω, U (X n ω) ∂P
         _ = a * ∫ᵉ ω, U (X n ω) ∂P + b * ∫ᵉ ω, U (X n ω) ∂P := by
           sorry
@@ -102,20 +132,28 @@ lemma exists_eq_iSup_eintegral_of_le' (hU_ccv : ConcaveOn ℝ≥0 Set.univ U)
           rw [← eintegral_mul_const (by simp) (by simpa),
             ← eintegral_mul_const (by simp) (by simpa)]
           rotate_left
-          · sorry -- eintegrable since U bounded above
-          · sorry -- same
+          · exact h_int _
+          · exact h_int _
           rw [eintegral_add]
           · simp
           · simp only [EReal.smul_ennreal_eq_mul]; fun_prop
           · simp only [EReal.smul_ennreal_eq_mul]; fun_prop
           · simp only [EReal.smul_ennreal_eq_mul]
-            refine eintegrable.const_mul ?_ (by simp) (by simpa)
-            sorry -- same
+            exact eintegrable.const_mul (h_int _) (by simp) (by simpa)
           · simp only [EReal.smul_ennreal_eq_mul]
-            refine eintegrable.const_mul ?_ (by simp) (by simpa)
-            sorry -- same
-          · right; sorry -- same
-          · left; sorry -- same
+            exact eintegrable.const_mul (h_int _) (by simp) (by simpa)
+          · right
+            refine (eintegral_lt_top_of_le (b := b • B) (fun ω ↦ ?_) ?_ P).ne
+            · simp only [EReal.smul_ennreal_eq_mul]
+              gcongr
+              exact hU_le _
+            · simp [EReal.mul_eq_top, hb_ne_top]
+          · left
+            refine (eintegral_lt_top_of_le (b := a • B) (fun ω ↦ ?_) ?_ P).ne
+            · simp only [EReal.smul_ennreal_eq_mul]
+              gcongr
+              exact hU_le _
+            · simp [EReal.mul_eq_top, ha_ne_top]
         _ ≤ ∫ᵉ ω, U (a * Y ω + b * Z ω) ∂P := by
           gcongr
           intro ω
@@ -136,7 +174,7 @@ lemma exists_eq_iSup_eintegral_of_le' (hU_ccv : ConcaveOn ℝ≥0 Set.univ U)
 is infinite. -/
 lemma exists_eq_iSup_eintegral_of_le (hU_ccv : ConcaveOn ℝ≥0 Set.univ U)
     {b : ℝ} (hU_cont : Continuous U) (hU_mono : Monotone U) (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
     ∃ Y : 𝓧 → ℝ≥0∞, IsEVar Y S ∧ ∀ X, IsEVar X S →
       (∫ᵉ x, U (X x) ∂P ≤ ∫ᵉ x, U (Y x) ∂P) ∧ (∀ᵐ x ∂P, Y x < ∞ → X x < ∞) := by
   obtain ⟨Y, hY_evar, h_opt⟩ := exists_eq_iSup_eintegral_of_le' hU_ccv hU_cont hU_le P S hS
@@ -174,25 +212,25 @@ end
 /-- The numeraire associated with a bounded utility function. -/
 noncomputable
 def numeraireOfBounded (U : Utility) {b : ℝ} (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
     𝓧 → ℝ≥0∞ :=
   (exists_eq_iSup_eintegral_of_le U.concave U.continuous U.monotone hU_le P S hS).choose
 
 lemma isEVar_numeraireOfBounded (U : Utility) {b : ℝ} (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
     IsEVar (numeraireOfBounded U hU_le P S hS) S :=
   (Classical.choose_spec
     (exists_eq_iSup_eintegral_of_le U.concave U.continuous U.monotone hU_le P S hS)).1
 
 lemma eintegral_le_numeraireOfBounded (U : Utility) {b : ℝ} (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ)
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ)
     {X : 𝓧 → ℝ≥0∞} (hX_evar : IsEVar X S) :
     ∫ᵉ x, U (X x) ∂P ≤ ∫ᵉ x, U (numeraireOfBounded U hU_le P S hS x) ∂P :=
   ((Classical.choose_spec
     (exists_eq_iSup_eintegral_of_le U.concave U.continuous U.monotone hU_le P S hS)).2 X hX_evar).1
 
 lemma lt_top_of_numeraireOfBounded_lt_top (U : Utility) {b : ℝ} (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ)
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ)
     {X : 𝓧 → ℝ≥0∞} (hX_evar : IsEVar X S) :
     ∀ᵐ x ∂P, (numeraireOfBounded U hU_le P S hS x) < ∞ → X x < ∞ :=
   ((Classical.choose_spec
@@ -200,20 +238,20 @@ lemma lt_top_of_numeraireOfBounded_lt_top (U : Utility) {b : ℝ} (hU_le : ∀ x
 
 -- first order optimality condition for bounded utility functions
 lemma eintegral_deriv_mul_le (U : Utility) {b : ℝ} (hU_le : ∀ x : ℝ≥0∞, U x ≤ b)
-    (P : Measure 𝓧) [SFinite P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ)
+    (P : Measure 𝓧) [IsFiniteMeasure P] (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ)
     {Y : 𝓧 → ℝ≥0∞} (hY : IsEVar Y S) :
     ∫ᵉ x, U.deriv (numeraireOfBounded U hU_le P S hS x)
       * (Y x - numeraireOfBounded U hU_le P S hS x) ∂P ≤ 0 := by
   sorry
 
 -- first order optimality condition for log utility
-lemma eintegral_deriv_log_mul_le (P : Measure 𝓧) [SFinite P]
+lemma eintegral_deriv_log_mul_le (P : Measure 𝓧) [IsFiniteMeasure P]
     (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
     ∃ Y : 𝓧 → ℝ≥0∞, IsEVar Y S ∧ ∀ X, IsEVar X S →
       ∫ᵉ x, logUtility.deriv (Y x) * (X x - Y x) ∂P ≤ 0 := by
   sorry
 
-lemma exists_numeraire' (P : Measure 𝓧) [SFinite P]
+lemma exists_numeraire' (P : Measure 𝓧) [IsFiniteMeasure P]
     (S : Set (Measure 𝓧)) (hS : ∀ μ ∈ S, IsFiniteMeasure μ) :
     ∃ Y : 𝓧 → ℝ≥0∞, IsEVar Y S ∧ ∀ X, IsEVar X S →
       ∫ᵉ x, (X x / Y x : ℝ≥0∞) - (Y x / Y x : ℝ≥0∞) ∂P ≤ 0 := by
